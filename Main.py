b2551ac8d27fa39d56b32081b928dbef1f2c4d80
sanidhya2013kvs <95016329+sanidhya2013kvs@users.noreply.github.com>
2024-08-22 01:19:04 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -3,25 +3,7 @@ from PIL import Image
 import time
 
 # Function to display the login form
-def login_form():
-    with st.form(key="login_form"):
-        login_id = st.text_input("Login ID")
-        password = st.text_input("Password")
-        login_button = st.form_submit_button("Login")
 
-        if login_button:
-            if login_id == "Sanidhya2468" and password == "pass2468":  # Replace with actual credentials
-                st.success("Logged in successfully")
-                
-            else:
-                st.error("Invalid Login ID or Password")
-
-# Check if the user is logged in
-if 'logged_in' not in st.session_state:
-    st.session_state.logged_in = False
-
-if not st.session_state.logged_in:
-    login_form()
 
     # Profile form for image upload and resize
 st.text("Name:Sanidhya Srivastava")
