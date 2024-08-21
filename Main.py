50f2a135c87fcdf6c6082c323f4e5a4fd4a0b29f
sanidhya2013kvs <95016329+sanidhya2013kvs@users.noreply.github.com>
2024-08-22 01:30:23 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,16 +4,15 @@ import time
 
 # Function to display the login form
 
+image_path = "./img.jpg"  # Replace with your image path
 
+# Display the image with a specified width
+st.image(image_path, width=150)  # Adjust width to your desired size
     # Profile form for image upload and resize
 st.text("Name:Sanidhya Srivastava")
 st.text("Position:computer vision research engineer")
 st.text("address:733, civil lines ,church road Sitapur UttarPradesh, 261001")
-st.text("phone number:+918840445270")
-st.text("UId:6753982561")
 
-if 'image_file' in st.session_state:
-    st.image(st.session_state.image_file, caption='Resized Passport Size Photo', use_column_width=True)
 
 if st.button("generate letter"):
     st.warning("Dear Candidate your's joining letter is in progress  please wait for few hours")
